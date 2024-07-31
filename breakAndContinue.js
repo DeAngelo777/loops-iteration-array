a99e5d515c964e0a7ae376cd4b10e18d63928000
Laimonas <ldirgelas530@gmail.com>
2024-07-31 13:32:47 +0300

    Initial commit

---
+++ breakAndContinue.js
@@ -0,0 +1,22 @@
+// // Break
+
+// for (let i = 0; i <=20; i++) {
+//     if (i === 10 ) {
+//         console.log('Breaking...');
+//         break;
+//     }
+//     console.log(i);
+
+// }
+
+
+// Continue
+
+for (let i = 0; i <=20; i++) {
+if (i === 8) {
+    console.log('Skipping 8...');
+    continue;
+
+}
+    console.log(i);
+}
